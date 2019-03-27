a132a186c3b1081cd325d4fc2fdb701676b92c3e
Pooya Nodehi <pooya.nodehi@gmail.com>
2019-03-27 12:04:45 +0100

    Update index.test.js
    initial commit

--- index.test.js
+++ index.test.js
@@ -9,6 +9,16 @@ describe("Calculator tests", () => {
     expect(new Calc(1).add(1).add(1)).toEqual({ num: 3 })
   })
 
+  test("should be able to multiply 1*2=2",()=>{
+    expect(new Calc(1).multiply(2)).toEqual({num:2})
+  })
+
+  test("should be able to divide 6/2=3",()=>{
+    expect(new Calc(6).divide(2)).toEqual({num:3})
+  })
+
+
+
   test("Dont accept strings", () => {
     expect(() => new Calc(100).add("onehundret")).toThrow("No strings allowed")
   })
